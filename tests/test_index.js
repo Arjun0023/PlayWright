c754d352830e5b83f3b065e59464418a16a23c2b
Arjun Pawar <108388620+Arjun0023@users.noreply.github.com>
2024-04-11 23:59:00 +0530

    test complete using json

--- tests/test_index.js
+++ tests/test_index.js
@@ -1,49 +1,32 @@
 const { chromium } = require('playwright');
-const data = require('./data.json'); // Assuming your JSON file is named data.json
+const fs = require('fs').promises;
 
 (async () => {
+  const data = await fs.readFile('data.json', 'utf-8');
+  const { email, password, companyName, lastName } = JSON.parse(data);
+
   const browser = await chromium.launch({
     headless: false
   });
   const context = await browser.newContext();
   const page = await context.newPage();
   await page.goto('https://www.google.com/search?q=zoho+crm+login&oq=zoho+crm+login&gs_lcrp=EgZjaHJvbWUyBggAEEUYOdIBCDc5ODVqMGoyqAIAsAIA&sourceid=chrome&ie=UTF-8');
-
   await page.getByRole('link', { name: 'Sign in to Zoho CRM Zoho' }).click();
-  await page.waitForNavigation(); // Wait for navigation to complete
-
   await page.getByRole('link', { name: 'SIGN IN' }).click();
-  await page.waitForNavigation(); // Wait for navigation to complete
-
-  // Fill in email
-  await page.getByPlaceholder('Email address or mobile number').fill(data.email);
+  await page.getByPlaceholder('Email address or mobile number').click();
+  await page.getByPlaceholder('Email address or mobile number').fill(email);
   await page.getByRole('button', { name: 'Next' }).click();
-  await page.waitForNavigation(); // Wait for navigation to complete
-
-  // Fill in password
-  await page.getByPlaceholder('Enter password').fill(data.password);
-  await page.press('Enter');
-  await page.waitForNavigation(); // Wait for navigation to complete
-
-  // Skip for now
+  await page.getByPlaceholder('Enter password').click();
+  await page.getByPlaceholder('Enter password').fill(password);
+  await page.getByPlaceholder('Enter password').press('Enter');
   await page.getByRole('button', { name: 'Skip for now' }).click();
-  await page.waitForNavigation(); // Wait for navigation to complete
-
-  // Go to Leads
   await page.getByRole('link', { name: 'Leads' }).click();
-  await page.waitForNavigation(); // Wait for navigation to complete
-
-  // Create lead
   await page.getByRole('button', { name: 'Create Lead' }).click();
-  await page.waitForNavigation(); // Wait for navigation to complete
-
-  // Fill lead information
-  await page.locator('#Crm_Leads_COMPANY #inputId').fill(data.company);
-  await page.locator('#Crm_Leads_LASTNAME_LInput').fill(data.lastName);
-
-  // Save lead
+  await page.locator('#Crm_Leads_COMPANY #inputId').click();
+  await page.locator('#Crm_Leads_COMPANY #inputId').fill(companyName);
+  await page.locator('#Crm_Leads_LASTNAME_LInput').fill(lastName);
   await page.getByRole('button', { name: 'Save and New' }).click();
-  
+
   // ---------------------
   await context.close();
   await browser.close();
